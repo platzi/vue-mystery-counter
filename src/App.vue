64962220f556c9dc4e1235213709efcc99309c70
edevars <e.devars.dev@gmail.com>
2025-01-20 16:39:09 -0600

    feat: adding new styles and structure

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,62 @@ import GameCounter from '@/components/GameCounter.vue'
 </script>
 
 <template>
-  <h1>Hola mundo</h1>
-  <GameCounter />
+  <main class="main">
+    <div class="wrapper">
+      <div class="wrapper__content">
+        <h1 class="title">Guess the number! 🎉</h1>
+        <span>{{ counter }}</span>
+        <div>
+          <GameCounter />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(to bottom right, #60fab2, #3b82f6, #2563eb);
+}
+
+.title {
+  text-align: center;
+  font-size: 3rem;
+  margin: 0;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
+.wrapper__content {
+  width: 100%;
+  max-width: 34rem;
+  margin: 0 auto;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  backdrop-filter: blur(16px);
+  background: linear-gradient(
+    to bottom right,
+    rgba(59, 130, 246, 0.3),
+    rgba(37, 99, 235, 0.3),
+    rgba(29, 78, 216, 0.3)
+  );
+  border-radius: 1.5rem;
+  box-shadow:
+    0 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  border: 1px solid rgba(191, 219, 254, 0.5);
+}
+</style>
--- src/components/GameCounter.vue
+++ src/components/GameCounter.vue
@@ -1,30 +1,73 @@
-<script setup>
-const isActive = false
-</script>
+<script setup></script>
 
 <template>
-  <button>menos</button>
-  <span class="counter" :class="{ 'counter--active': isActive }" style="font-size: 4rem"
-    >contador: <span>0</span></span
-  >
-  <button>mas</button>
+  <div class="counter-game">
+    <span class="number">0</span>
+    <div class="button-group">
+      <button>-</button>
+      <button>+</button>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
-.counter {
+.counter-game {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+}
+
+.number {
+  font-size: 5rem;
   font-weight: bold;
-  font-size: 2rem;
-
-  & > span {
-    color: green;
-  }
+  color: white;
+  opacity: 0.9;
+  transition: all 0.3s ease-in-out;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  text-align: center;
 }
 
-.counter--active {
-  color: aqua;
+.number:hover {
+  transform: scale(1.1);
+  color: rgb(219, 234, 254);
 }
 
+.button-group {
+  display: flex;
+  gap: 1rem;
+}
+
+.clue {
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  border-radius: 50%;
+  width: 2rem;
+  height: 2rem;
+  background-color: transparent;
+  color: white;
+  border: 2px solid white;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+/* Estilos botones */
 button {
-  background: red;
+  width: 4rem;
+  height: 4rem;
+  font-size: 2rem;
+  border-radius: 1rem;
+  background-color: rgba(119, 170, 251, 0.914);
+  color: white;
+  border: none;
+  box-shadow:
+    0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  cursor: pointer;
+}
+
+button:hover:not(:disabled) {
+  background-color: rgba(26, 77, 188, 0.7);
 }
 </style>
