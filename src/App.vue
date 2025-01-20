2254a8894c99c202cfee72504c4c0d1e51f12ac3
edevars <e.devars.dev@gmail.com>
2025-01-20 16:05:28 -0600

    feat: sfc vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import GameCounter from '@/components/GameCounter.vue'
+</script>
 
 <template>
   <h1>Hola mundo</h1>
+  <GameCounter />
 </template>
 
 <style scoped></style>
---
+++ src/components/GameCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+console.log('Componente de juego')
+</script>
+
+<template>
+  <button>menos</button>
+  <span>contador: 0</span>
+  <button>mas</button>
+</template>
+
+<style>
+button {
+  background: red;
+}
+</style>
